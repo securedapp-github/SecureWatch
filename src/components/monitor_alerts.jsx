f5f8aea205a13be3c2e62baeabded628e7201cfe
Naveen-gn <naveen2003gn@gmail.com>
2024-06-17 17:13:14 +0530

    dashboard, alert page modifications

--- src/components/monitor_alerts.jsx
+++ src/components/monitor_alerts.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from "react";
 import { useLocation } from 'react-router-dom';
 import Navbar from "./navbar2";
+import { FaCaretDown } from 'react-icons/fa';
 
 
 
@@ -10,6 +11,9 @@ function Monitor_alerts() {
   console.log("transfered mid",mid);
   const  [alert,setAlert] = useState([]);
   const email = localStorage.getItem("email")
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeDiv, setActiveDiv] = useState(null);
+
   useEffect(() => {
     const fetchAlert = async () => {
       const res=await fetch('https://139-59-5-56.nip.io:3443/get_alerts',{
@@ -27,6 +31,8 @@ function Monitor_alerts() {
     fetchAlert();
     console.log("alert",alert);
   }, []);
+
+
   if (!alert.alerts||alert.alerts.length===0  ||  alert===undefined){
     return (
       <div className="pt-10 bg-white">
@@ -42,7 +48,7 @@ function Monitor_alerts() {
       <div className="text-xl lg:text-3xl font-medium text-black text-center mt-10">Your Monitor Alerts </div>
 
       <div>
-      {alert.alerts && alert.alerts.map((alert) => {
+      {alert.alerts && alert.alerts.map((alert,index) => {
         const id = alert.id;
         const hash = alert.hash;
         const arguemant = alert.arguments;
@@ -52,7 +58,7 @@ function Monitor_alerts() {
         const eid = alert.eid;
         return (
           <div className="">
-            <div className="w-[95%] lg:w-4/6 mx-auto  flex flex-col mb-14 mt-10">
+            <div key={index} className="w-[95%] lg:w-4/6 mx-auto  flex flex-col mb-14 mt-10 ">
 
             <div className="w-full flex gap-4 flex-wrap">
 
@@ -60,11 +66,38 @@ function Monitor_alerts() {
                      Hash : <span className="text-lg  text-black ">{`${hash.slice(0, 5)}...${hash.slice(hash.length - 4)}`}</span>
                     </div>
 
-                    <div className="flex gap-2 items-center text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300">
+{/* <button onClick={()=>{
+  setIsMenuOpen(!isMenuOpen);
+}} className="flex relative gap-2 items-center text-xl font-semibold text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300">Created on <FaCaretDown />
+{isMenuOpen && (
+  <div className="absolute z-10 mt-24 mr-11">
+    <div className="flex gap-2 items-center text-xl font-semibold text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300 text-nowrap">{created_on.slice(0,10)} {created_on.slice(11,16)}</div>
+  </div>
+)}
+</button>  */}
+
+<button onClick={() => setActiveDiv(activeDiv === index ? null : index)} className="flex relative gap-2 items-center text-xl font-semibold text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300">
+    Created on <FaCaretDown />
+    {activeDiv === index && (
+      <div className="absolute z-10 mt-24 -left-1">
+        <div className="flex gap-2 items-center text-xl font-semibold text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300 text-nowrap">{created_on.slice(0,10)} {created_on.slice(11,16)}</div>
+      </div>
+    )}
+</button>
+
+
+
+{/* <select name="Created on" className=" text-xl font-semibold text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300 focus:border-gray-300 active:border-gray-300 checked:border-gray-300 visited:border-gray-300">
+  <option hidden>Created On</option>
+  <option >{created_on.slice(0,10)} {created_on.slice(11,16)}</option>
+</select> */}
+                    {/* <div className="flex gap-2 items-center text-gray-500 bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300">
+
                       <div className=" text-xl font-semibold text-gray-500">Created on:</div>
                         <div className="my-auto text-md font-medium text-black">{created_on.slice(0,10)} {created_on.slice(11,16)}
                       </div>
-                    </div>
+                      
+                    </div> */}
 
                     <div className="text-xl font-semibold text-gray-500  bg-[#E9E9E9] px-3 py-2 rounded-xl border-2 border-gray-300">From: <span className="text-lg mt-auto text-black">{`${from_address.slice(0, 5)}...${from_address.slice(from_address.length - 4)}`}</span> </div>
 
@@ -122,6 +155,7 @@ function Monitor_alerts() {
         );
       })}
     </div>
+
     </div>
   )
 }
