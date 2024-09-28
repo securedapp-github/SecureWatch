4aa7868c7582376c69b91e7b0645d13218d441d5
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2024-09-28 13:22:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ import Forgotpassword from "./components/Forgotpassword.jsx";
 import Otpverify from "./components/Otpverify.jsx";
 import 'react-toastify/dist/ReactToastify.css';
 import { toast } from "react-toastify";
+import AlgoEvents from "./components/algoevents.jsx";
+import AlgoEventsedit from "./components/algoevents_edit.jsx";
 
 function App() {
   return (
@@ -50,6 +52,11 @@ function App() {
         <Route path="/monitor" element={<Protected cmp={<Monitor />} />} />
         <Route path="/api_builder" element={<Protected cmp={<ApiBuilder />} />}/>
         <Route path="/monitor_alerts" element={<MonitorAlerts />} />
+         <Route
+          path="/AlgoEventsedit"
+          element={<Protected cmp={<AlgoEventsedit />} />}
+        />
+         <Route path="/Algoevents" element={<AlgoEvents />} />
       </Routes>
     </BrowserRouter>
   );
