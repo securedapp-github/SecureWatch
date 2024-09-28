b8003023ae8dc7a884a485e278d721f8246dab6a
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2024-09-28 13:39:33 +0530

    Algo integration

--- src/components/monitor_cmp.jsx
+++ src/components/monitor_cmp.jsx
@@ -137,6 +137,10 @@ const Monitor_cmp = () => {
                             ? "Sepolia Testnet"
                             : network === 137
                             ? "Polygon Mainnet"
+                            : network === 1300
+                            ? "Algorand Mainnet"
+                            : network === 1301
+                            ? "Algorand Testnet"
                             : "Unknown"}
                         </div>
                       </div>
