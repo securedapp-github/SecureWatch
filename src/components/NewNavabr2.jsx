cbbaf9fb59fb50805b76136aff79c862d5b4b6b8
Naveen-gn <naveen2003gn@gmail.com>
2025-01-14 21:24:38 +0530

    Small fix in support popup

--- src/components/NewNavabr2.jsx
+++ src/components/NewNavabr2.jsx
@@ -13,10 +13,13 @@ import { IoPersonSharp } from "react-icons/io5";
 import { LuNetwork } from "react-icons/lu";
 import SecureDapp from "../images/SecureDapp.png";
 import { Link } from 'react-router-dom';
+import { MdPhoneIphone } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
 
 export default function NewNavbar({ email }) {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [expandRealtimeSecurity, setExpandRealtimeSecurity] = useState(false);
+  const [isSupportMenuOpen, setIsSupportMenuOpen] = useState(false);
   
   return (
     <div className=" bg-white w-full px-4 py-5 flex justify-between items-center fixed z-50">
@@ -24,10 +27,17 @@ export default function NewNavbar({ email }) {
       <h1 className="logo text-black text-xl sm:text-3xl">Securewatch</h1>
      
       <div className="flex items-center gap-7  ">
-        <button className="p-0">
+        <button className="p-0" onClick={()=>setIsSupportMenuOpen(!isSupportMenuOpen)}>
           <BsHeadset className="text-[#535252] text-2xl" />
         </button>
       </div>
+      {isSupportMenuOpen && (
+          <div className='  absolute customShadow z-50 right-12 mt-32   bg-white rounded-md  py-3 w-56 flex flex-col justify-center items-start gap-3'>    
+              <p className='text-black flex gap-3 items-center text-sm pl-5'> <MdPhoneIphone className='text-black text-sm' /> 9606015868</p>
+              <div className='w-full h-[0.5px] bg-slate-300'></div>
+              <p className='text-black flex gap-3 items-center text-sm pl-5'><MdOutlineEmail className='text-black text-sm' /> hello@securedapp.in</p>
+          </div>)  
+        }
 
     </div>
   );
